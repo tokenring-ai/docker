4d62743b0e44a4cdd60061cb4ed70a21035d00e7
Mark Dierolf <mdierolf@cloudquote.io>
2025-09-28 08:25:29 -0700

    Renamed `DockerSandboxResource` to `DockerSandboxProvider` in exports for consistency.

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import packageJSON from './package.json' with {type: 'json'};
 import * as tools from "./tools.ts";
 
 export {default as DockerService} from "./DockerService.ts";
-export {default as DockerSandboxResource} from "./DockerSandboxProvider.ts";
+export {default as DockerSandboxProvider} from "./DockerSandboxProvider.ts";
 
 export const packageInfo: TokenRingPackage = {
   name: packageJSON.name,
