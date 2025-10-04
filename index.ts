a224b98a19582cc00186525c9c9ef975617c1b31
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-04 12:50:36 -0700

    Implemented auto-configuration, that allows the package to automatically install into TokenRing apps

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
-import {TokenRingPackage} from "@tokenring-ai/agent";
+import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
+import {z} from "zod";
+import DockerService from "./DockerService.ts";
 import packageJSON from './package.json' with {type: 'json'};
 import * as tools from "./tools.ts";
 
-export {default as DockerService} from "./DockerService.ts";
-export {default as DockerSandboxProvider} from "./DockerSandboxProvider.ts";
+export const DockerConfigSchema = z.any().optional();
 
 export const packageInfo: TokenRingPackage = {
   name: packageJSON.name,
   version: packageJSON.version,
   description: packageJSON.description,
-  tools
+  install(agentTeam: AgentTeam) {
+    const config = agentTeam.getConfigSlice('docker', DockerConfigSchema);
+    if (config) {
+      agentTeam.addTools(packageInfo, tools);
+      agentTeam.addServices(new DockerService(config));
+    }
+  }
 };
+
+export {default as DockerService} from "./DockerService.ts";
+export {default as DockerSandboxProvider} from "./DockerSandboxProvider.ts";
