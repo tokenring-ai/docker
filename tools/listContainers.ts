706633184e77defca12d8db765b22d4530b2d0f4
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:55:42 -0800

    Updated files to use `satisfies` for type assertion

--- tools/listContainers.ts
+++ tools/listContainers.ts
@@ -182,4 +182,4 @@ const inputSchema = z.object({
 
 export default {
   name, description, inputSchema, execute,
-} as TokenRingToolDefinition<typeof inputSchema>;
+} satisfies TokenRingToolDefinition<typeof inputSchema>;
